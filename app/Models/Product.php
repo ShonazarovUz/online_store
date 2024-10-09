501b1e70ce6ca03bab606166c5f0b8e3f657578a
ShonazarovUz <shonazarovnasriddinuz@gmail.com>
2024-10-09 19:54:00 +0500

    Add Api Add user, show users and show user

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,25 +8,31 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+
+    // Fillable fields for mass assignment
     protected $fillable = [
-        'name', 'description', 'price', 'stock', 'category_id', 'vendor_id'
+        'name', 'description', 'price', 'stock', 'category_id', 'vendor_id',
     ];
 
+    // Relationship with the Category model
     public function category()
     {
         return $this->belongsTo(Category::class);
     }
 
-    public function user()
+    // Relationship with the User model (vendor)
+    public function vendor()
     {
         return $this->belongsTo(User::class, 'vendor_id');
     }
 
+    // Relationship with the Comment model
     public function comments()
     {
         return $this->hasMany(Comment::class);
     }
 
+    // Relationship with the Image model
     public function images()
     {
         return $this->hasMany(Image::class);
